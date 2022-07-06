1c2a943b906b20c217a8f9ba99ac52d40b55d20a
Ploychom Treekunprapha <ploychomtreekunprapha@Ploychoms-MacBook-Air.local>
2022-07-06 18:18:54 +0700

    fixed bug

--- js/firebase.js
+++ js/firebase.js
@@ -11,6 +11,21 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
 
+//
+let popup = document.getElementById("popup");
+let ovl = document.getElementById("overlay");
+
+function openPopup() {
+    popup.classList.add("open-popup");
+    ovl.classList.add("ovl");
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+    ovl.classList.remove("ovl");
+}
+//
+
 document.getElementById("form").addEventListener("submit", submitForm);
 
 function submitForm(e){
